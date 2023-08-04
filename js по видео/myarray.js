f0f8374251ad2188cca556d11df898f7fbc75d25
Vlad Pigotskij <belarus0000@gmail.com>
2023-08-04 17:11:33 +0300

    метод map and forEach

--- js по видео/myarray.js
+++ js по видео/myarray.js
@@ -1,6 +1,25 @@
+// метод forEach
+console.log('метод forEach')
 const myArray = [1, 2, 3, 10]
-console.log(myArray)
+console.log("массив myArray:", myArray)
+// метод forEach возвращает только undefined
+// возврат функции не явный. т.к. нет фигурных скобок.
+console.log('умножаем на 2, каждый элемент массива:')
 const res = myArray.forEach(el => console.log(el*2))
-console.log(res)
-console.log(myArray)
+console.log("возврат значения метода forEach:", res)
+console.log("массив myArray не изменился:", myArray)
+
+// метод MAP
+// возвращает новый массив, перебирает все элементы массива и
+// вызывает колбэк функции количество раз, в зависимости от
+// количества элементов массива
+console.log('метод MAP')
+const myArray2 = [1, 2, 3, 20]
+console.log("массив myArray2:", myArray2)
+const newArray2 = myArray2.map(el => el * 3)
+console.log("результат умнажения методом map, массив newArray:",
+newArray2)
+console.log("массив myArray2:", myArray2)
+console.log("массив newArray2:", newArray2)
+// создался новый массив newArray2.
 
