620c3b014e2bfe6d39386e57f8f187536632412a
Vlad Pigotskij <belarus0000@gmail.com>
2023-09-11 12:14:08 +0300

    базовый калькулятор

--- codewars/StripComments.js
+++ codewars/StripComments.js
@@ -33,4 +33,12 @@ function solution(input, markers) {
     });
     return str.replace(/\n$/, '')
 }
+// не мое решение:
+// function solution(input, markers) {
+//     return input.split('\n').map(
+//       line => markers.reduce(
+//         (line, marker) => line.split(marker)[0].trim(), line
+//       )
+//     ).join('\n')
+// }
 console.log(solution('apples, pears # and bananas\ngrapes\nbananas !apples', ['#', '!']))
