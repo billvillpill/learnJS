fcfd226d846829fbef7b87ca696e91a83d369fdc
Vlad Pigotskij <belarus0000@gmail.com>
2023-09-20 13:31:47 +0300

    проверка первого элемента массива на четность

--- repeat-string.js
+++ repeat-string.js
@@ -1,7 +1,15 @@
-function truncateString(str, num) {
-    num >= str.length ? str : str = str.split('').slice(0, num).join('') + '...';
-    return str;
+// вернуть первое число из массива arr, которое соответсвует истене (true) в результате функции func,
+// которая передается в качестве аргумента.
+// Если все элементы массива - в результате функции func возвращают false. тогда вернуть undefined
+function findElement(arr, func) {
+    let num;
+    let i = 0;
+    do {
+        num = arr[i] 
+        func(num) ? num : num = undefined
+        i += 1;
+    } while (func(num) === false && i < arr.length);
+    return num;
 }
-  
-console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
-Array.prototype.split
+
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
